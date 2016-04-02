3c5a6f91ff1c84c6a3b582dd5eb64641116c3890
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2016-04-01 19:59:06 -0700

    Adds global components/routes/store modules for the app

--- src/routes/about/components/AboutComponent.tsx
+++ src/routes/about/components/AboutComponent.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Link } from 'react-router';
-import Header, { HeaderKind } from '../../../components/HeaderComponent';
-import Banner, { BannerKind } from '../../../components/BannerComponent';
+import Header, { HeaderKind } from 'components/HeaderComponent';
+import Banner, { BannerKind } from 'components/BannerComponent';
 
 export default class AboutComponent extends React.Component<any, any> {
   render() {
